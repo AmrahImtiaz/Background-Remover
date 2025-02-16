d134de6cee2dbf668bc9dfab31ac9f809856dac5
AmrahImtiaz <amrahimtiazgame@gmail.com>
2025-02-16 15:25:50 +0500

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Header from '../components/Header'
 import Steps from '../components/Steps'
 import BgSlider from '../components/BgSlider'
+import Upload from '../components/Upload'
+
 
 const Home = () => {
   return (
@@ -9,6 +11,7 @@ const Home = () => {
       <Header/>
       <Steps/>
       <BgSlider/>
+      <Upload/>
     </div>
   )
 }
